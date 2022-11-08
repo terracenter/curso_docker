f61754b78ebf28788e6423be9749dca72405f8ea
Freddy Taborda <terracenter@gmail.com>
2022-11-08 14:34:36 -0400

    Add Cotinuo practicanddo

--- debian-nginx-php8.1-fpm/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-apt update && apt upgrade -y && apt install -y curl &&\
-curl -sSL https://packages.sury.org/php/README.txt | bash -x &&\
-apt update && apt install -y nginx &&\
-apt install -y php-fpm php8.1-zip php8.1-intl php8.1-xml \
-php8.1-curl php8.1-mbstring php8.1-gd  
